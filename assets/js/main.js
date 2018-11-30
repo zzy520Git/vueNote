9ebcb9c0d1bc374ba5707529ceb8fb624c123af0
zhouzhongyi1 <zhouzhongyi1@jd.com>
2018-11-30 20:42:06 +0800

    ==

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//vue.min.js
+document.write('<script type="text/javascript" src="/vueNote/assets/js/vue/vue.min.js"></script>');
+//jquery
+document.write('<script type="text/javascript" src="/vueNote/assets/js/jquery/jquery-1.10.2.min.js"></script>');
+//bootstrap.min.js
+document.write('<script type="text/javascript" src="/vueNote/assets/js/bootstrap/bootstrap.min.js"></script>');
