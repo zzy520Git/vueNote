4050b9ff3f72b03e23831ecab753801a16dbe340
zhouzhongyi1 <zhouzhongyi1@jd.com>
2018-12-14 19:45:09 +0800

    日期格式化+分页和列表

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,7 @@ document.write('<script type="text/javascript" src="/vueNote/assets/js/vue/vue.m
 document.write('<script type="text/javascript" src="/vueNote/assets/js/jquery/jquery-1.10.2.min.js"></script>');
 //bootstrap.min.js
 document.write('<script type="text/javascript" src="/vueNote/assets/js/bootstrap/bootstrap.min.js"></script>');
+//时间日期处理
+document.write('<script type="text/javascript" src="/vueNote/assets/js/moment/moment.js"></script>');
+
+
